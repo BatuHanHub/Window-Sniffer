0f53d169a3ff3fbb52fbe6ed58668e6504eb4c6b
BatuHanHub <114951572+BatuHanHub@users.noreply.github.com>
2022-12-25 13:52:25 +0300

    Uygulama adı + Açıldığı tarih eklendi

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import os # terminali temizlemek için
 import win32gui # uygulama adlarını görmek için
 import time # sayaç tutmak için
+import datetime # uygulamanın açıldığı tarihi görmek için
 
 # eğer programımızı Windows ve Linux/Unix için yazsaydık ve terminali temizlemek isteseydik şu kodu yazmamız gerekirdi;
 '''
@@ -18,17 +19,22 @@ elif degisken == "poxit": # Linux/Unix (BSD,macOS vs...) için
 # NOT: Ekranınıza sakın bal dökmeyin :D
 '''
 
+dosya = open("Geçmiş.txt", "a", encoding='utf-8')
+dosya.write ("\t========GEÇMİŞ========\n\n")
+dosya.close()
+
 def aktifPencereBaslik(): # 'aktifPencereBaslik' adında fonksiyon oluşturuldu 
     return win32gui.GetWindowText(win32gui.GetForegroundWindow()) # açılan pencerenin adını döndürür
 
 while True: # program döngüde
     
   Baslik = aktifPencereBaslik() # 'Baslik' değişkeni 'aktifPencereBaslik' değişkenine aktarıldı
+  Tarih = datetime.datetime.now()
   
   print(Baslik) # ekrana pencereyi yazdı
   time.sleep(1.5) # 1.5 saniye bekledi
   os.system('cls') # terminali temizledi  NOT!:Sadece Windows'da çalışır Linux veya Unix(macOS, BSD vs...) sistemler için 'clear' kullanılır.
   
   dosya = open("Geçmiş.txt", "a", encoding='utf-8') # 'Geçmiş.txt' dosyası oluşturdu
-  dosya.write (f"{Baslik}\n") # uygulamaları yazdı
+  dosya.write (f"{Baslik} = ({Tarih.strftime('%H.%M.%S')})\n") # uygulamaları adlarını ve açıldığı tarihler yazıldı
   dosya.close # işi bitince kapattı 
