4250181c08031a19de58f41bb707c0e354a7c957
BatuHanHub <114951572+BatuHanHub@users.noreply.github.com>
2023-01-22 02:20:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 #KÜTÜPHANELER
-import datetime # uygulamanın açıldığı tarihi görmek için
-import win32gui # uygulama adlarını görmek için
-import time # sayaç tutmak için
+import datetime # Tarih kütüphanesi
+import win32gui # Başlık için kütüphane
+import time # Zaman kütüphanesi
 
 def aktifPencereBaslik(): # 'aktifPencereBaslik' adında fonksiyon oluşturuldu 
-    return win32gui.GetWindowText(win32gui.GetForegroundWindow()) # açılan pencerenin adını döndürür
+    return win32gui.GetWindowText(win32gui.GetForegroundWindow()) # Açılan pencerenin adını döndürür
 
-enSonBaslik = "" # bu değişkeni aşağıda açıklayacağım
+enSonBaslik = "" # Bu değişken En son açılan pencereyi tutuyor
 
-while True: # program döngüde
-  time.sleep(0.01) # program işlemcinin içinden geçmemesi için 0.01 zaman bekliyor
+while True: # Program döngüde
+  time.sleep(0.01) # Program işlemciyi yormaması için 0.01 saniye bekliyor
   Baslik = aktifPencereBaslik() # 'aktifPencereBaslik' fonksiyonu 'Baslik' değişkenine aktarıldı
-  Tarih = datetime.datetime.now() # tarih zaman değişkeni
+  Tarih = datetime.datetime.now() # Tarih değikeni zaman değişkenini tutuyor
   
   if Baslik != enSonBaslik: # eğer başlık sonbaslık değilse : 
-        print(f"- {Baslik} [{Tarih.strftime('%x %X')}]") # değişken ekrana yazdırılır
+        print(f"- {Baslik} programı {Tarih.strftime('%x %X')} zamanında başladı.\n") # değişken ekrana yazdırılır
         enSonBaslik = Baslik # ensonbaşlık başlık değişkenine eklendi
   
-        dosya = open("log.txt", "a", encoding='utf-8') # 'Geçmiş.txt' dosyası oluşturdu
-        dosya.write (f"- {Baslik} programı {Tarih.strftime('%x %X')} zamanında başladı.\n")
+        dosya = open("log.txt", "a", encoding='utf-8') # 'log.txt' dosyası oluşturdu
+        dosya.write (f"- {Baslik} programı {Tarih.strftime('%x %X')} zamanında başladı.\n") 
         dosya.close # işi bitince kapattı
